1aa149f55b4e4fa2c99179c214daa984fb374a05
vvshadow <zayd.benoumeur@gmail.com>
2024-11-14 14:18:39 +0100

    modif back color       font size -->12px

--- App.js
+++ App.js
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#afff33',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: '12px',
+    
   },
 });
