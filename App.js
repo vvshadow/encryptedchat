d0f52cb092763c78fc10de4a98f099367054d66c
vvshadow <zayd.benoumeur@gmail.com>
2024-11-14 16:36:45 +0100

    random

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert, Randomiser, TextInput } from 'react-native';
+
+
 
 export default function App() {
+  const [nb, setNb] = useState(0);
+
+  const plus = () => {
+    setNb(nb + 1);
+  }
+
+  const moins = () => {
+    setNb(nb - 1)
+  }
+  const [number, onChangeNb] = React.useState('');
+
+  let max = 100;
+  let min = 1;
+  let rand = Math.floor(Math.random() * (max - min));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Text}>
+        App Zayd React native</Text>
       <StatusBar style="auto" />
+      <Image source={require('./assets/favicon.png')}></Image>
+      <Button onPress={() => Alert.alert('Vous avez cliquez')} title="React" color="#ff0000"
+        accessibilityLabel="test de test de test" />
+      <Text>valeur de {nb}</Text>
+      <TextInput
+      style={styles.input}
+      onChanheText={onChangeNb}
+      value={number}
+      placeholder='Entrer un nombre'
+      keyboardType='numeric'
+      ></TextInput>
+      <Text>nombre magique : {rand}</Text>
+      <Button onPress={plus} title="plus" color="#ff0000"
+        accessibilityLabel="testplus" />
+
+      <Button onPress={moins} title="moins" color="#ff0000"
+        accessibilityLabel="testplus" />
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +56,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#afff33',
     alignItems: 'center',
     justifyContent: 'center',
-    fontSize: '12px',
-    
+
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  statusbar: {
+    color: '#ff0000',
+  },
+  Text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  }
 });
