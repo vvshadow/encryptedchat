caa152cb127ec096b2b7d1af18e30caef490fe89
vvshadow <zayd.benoumeur@gmail.com>
2024-12-09 20:58:50 +0100

    redirection automatique a la page register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 // frontend/src/App.jsx
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat'; // Page du chat après connexion
 import Login from './pages/Login'; // Page de connexion
 import Register from './pages/Register'; // Page d'inscription
+import Profil from './pages/Profil'; // Page du profil
 import "./App.css";
 import { IoIosAddCircle, IoMdHeart } from "react-icons/io";
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/profil" element={<Profil />} />
         {/* Autres routes */}
       </Routes>
     </Router>
