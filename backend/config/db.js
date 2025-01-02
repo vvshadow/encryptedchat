53888c481e02566d5743d06066da8b8530da2258
vvshadow <zayd.benoumeur@gmail.com>
2025-01-02 20:28:02 +0100

    connect to mongodb atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/encryptedchat', {
+    await mongoose.connect('mongodb+srv://lucaskurek2:lucaskurek2@cluster0.nrfn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
